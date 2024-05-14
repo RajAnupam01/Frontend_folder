795549bfab3344cf49e6ba8849472dd2479cdbc2
Anupam Raj <anupam.r2301@gmail.com>
2024-05-14 12:31:53 +0530

    Add initial files

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 15;
+let b = 25;
+
+console.log(`Before Swapping The value of a:${a} and b:${b}`);
+
+let temp;
+temp=a;
+a=b;
+b=temp;
+
+console.log(`After Swapping The value of b:${a} and b:${b}`);
